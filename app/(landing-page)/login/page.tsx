7c95866bb299427f3e1b0e3df3615c3bc7e372f5
Maryemelb <maryemelbergui@gmail.com>
2026-02-22 09:15:10 +0000

    create a landing page

---
+++ app/(landing-page)/login/page.tsx
@@ -0,0 +1,13 @@
+
+'use client';
+import LoginForm from "../../components/LoginForm";
+
+export default function Login() {
+    const is_login=true
+return(
+    <div>
+        login
+        <LoginForm is_login={is_login}></LoginForm>
+    </div>
+)
+}
